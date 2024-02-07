8e47b89c66ea08f992b4bc22443013b69e317eb2
ludio_boi <fred@wilds-online.de>
2024-02-07 11:35:16 +0100

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 let mysql
 let express
 let logger
+let database
 
 try {
     mysql = require("mysql")
@@ -27,51 +28,150 @@ const DATABASE_STATES = {
     connected: "authenticated",
     disconnected: "disconnected"
 }
-
+const config = {
+    host: "localhost",
+    port: MYSQL_PORT,
+    user: "root",
+    database: "sas"
+}
 
 const app = express()
+app.use(express.json())
 
-const database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "sas",
-})
-
-database.connect((error) => {
-    if (error){
-        logger.log_error(error.stack)
-    }
-    logger.log_info("Connected with thread id " + database.threadId)
-})
-
-let bla = {
-    name: "bla",
-    classID: 1
+function initMySQL() {
+    database.query("CREATE DATABASE IF NOT EXISTS sas")
+    database.query("USE sas")
+    database.query("CREATE TABLE IF NOT EXISTS students (ID int PRIMARY KEY, firstname TINYTEXT, middlename TINYTEXT, lastname TINYTEXT, classID int)", (error, results, fields) => {})
+    database.query("CREATE TABLE IF NOT EXISTS classes (classID INT PRIMARY KEY, shortname VARCHAR(20), longname VARCHAR(100), teacherID INT)");
 }
-database.query("CREATE TABLE IF NOT EXISTS students (ID int PRIMARY KEY, firstname TINYTEXT, middlename TINYTEXT, lastname TINYTEXT, classID int)", (error, results, fields) => {})
-database.query("CREATE TABLE IF NOT EXISTS classes (classID INT PRIMARY KEY, shortname VARCHAR(20), longname VARCHAR(100), teacherID INT)");
+
+function query(query, values=[]){
+    return new Promise((resolve, reject) => {
+        database.query(query, values, (error, results, fields) => {
+            if (error) {
+                reject({code: 503, error: error});
+                return
+            }
+            resolve(results, fields)
+        })
+    })
+}
+
+database = mysql.createPool(config)
+database.on('connection', (connection) => {
+    logger.log_info("Connected with thread id " + connection.threadId)
+    connection.on('error', (error) => {
+        logger.log_error("Error on thread id " + connection.threadId + ": " + error)
+    })
+})
 
 
-app.get("/students/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  database.query(`SELECT * FROM students WHERE id = ?`, [id], (error, results, fields) => {
-    if (error) {
-      res.status(503).send({
-        message: error.message
-      });
-      return;
-    }
+app.get("/students/:id", (request, response) => {
+  const id = parseInt(request.params.id);
 
-    if (results.length === 0) {
-      res.status(404).send({
-        message: "Student not found"
-      });
-      return;
-    }
 
-    res.send(results[0]);
-  });
+  query(`SELECT * FROM students WHERE ID =?`, [id]).then((results, fields) => {
+      if (results.length === 0) {
+          response.status(404).send({error: "Entry not found"})
+          return
+      }
+      response.send(results[0]);
+  }).catch((error) => {
+      response.status(error.code).send({error: error.error});
+  })
 });
+
+app.get("/classes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  query(`SELECT * FROM classes WHERE classID = ?`, [id]).then((results, fields) => {
+      if (results.length === 0) {
+          res.status(404).send({error: "Entry not found"})
+          return
+      }
+      res.send(results[0]);
+  }).catch(error => {
+      res.status(error.code).send({error: error.error});
+  })
+});
+
+app.get("/classes", (req, res) => {
+    let limit = "", offset = "";
+    if (req.query.limit) {
+        limit = "LIMIT " + parseInt(req.query.limit)
+    }
+    if (req.query.offset) {
+        offset = "OFFSET " + parseInt(req.query.offset)
+    }
+
+    let queryString = "SELECT * FROM students " + limit + " " + offset;
+    query(queryString.replaceAll("  ", " ")).then((results, fields) => {
+        if (results.length === 0) {
+            res.status(404).send({error: "Entry not found"})
+            return
+        }
+        res.send(results);
+    }).catch((error) => {
+        res.status(error.code).send({error: error.error});
+    })
+})
+
+app.get("/students", (req, res) => {
+    let limit = "", offset = "";
+    if (req.query.limit) {
+        limit = "LIMIT " + parseInt(req.query.limit)
+    }
+    if (req.query.offset) {
+        offset = "OFFSET " + parseInt(req.query.offset)
+    }
+    let queryString = "SELECT * FROM students " + limit + " " + offset;
+    query(queryString.replaceAll("  ", " ")).then((results, fields) => {
+        if (results.length === 0) {
+            res.status(404).send({error: "Entry not found"})
+            return
+        }
+        res.send(results);
+    }).catch((error) => {
+        res.status(error.code).send({error: error.error});
+    })
+});
+
+app.put("/students", (req, res) => {
+    if (!req.is("application/json")){
+        res.status(415).send({error: "Content type must be application/json"})
+        return
+    }
+    let body = req.body;
+    if (body === null){
+        res.status(400).send({error: "Body cannot be empty"})
+        return
+    }
+    logger.log_info(body)
+    if (!(Array.isArray(body))){
+        res.status(400).send({error: "Body must be an array"})
+        return
+    }
+
+    let error = false;
+
+    for (let student of body) {
+        if (!(student instanceof Object)){
+            res.status(400).send({error: "Body must be an array of objects"})
+            return
+        }
+        if (student.firstname === undefined || student.ID === undefined || student.lastname === undefined || student.class === undefined){
+            res.status(400).send({error: "Firstname, Lastname, ID and ClassID are required"})
+            return
+        }
+        query("INSERT INTO students (id, firstname, middlename, lastname, class) VALUES (?,?,?,?,?)", [student.id, student.firstname, student.middlename, student.lastname, student.class]).catch(error => {
+            res.status(error.code).send({error: error.error})
+            return
+        })
+    }
+    res.status(200).send({message: "Students added successfully"})
+
+})
+
+
 app.listen(8080)
 
